54c00cf1c0c180c57f7f6adbfd2fc01feb5558fe
ezgiorman <ezgiorman10@gmail.com>
2026-03-12 21:21:42 +0300

    entities added.

--- InsureYouAI/Entities/PricingPlan.cs
+++ InsureYouAI/Entities/PricingPlan.cs
@@ -5,5 +5,6 @@
         public int PricingPlanId { get; set; }
         public string Title { get; set; }
         public decimal Price { get; set; }
-    }
+        public bool IsFeature { get; set; }
+}
 }
---
+++ InsureYouAI/Migrations/20260312182107_mg5.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsureYouAI.Migrations
+{
+    /// <inheritdoc />
+    public partial class mg5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFeature",
+                table: "PricingPlans",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFeature",
+                table: "PricingPlans");
+        }
+    }
+}
